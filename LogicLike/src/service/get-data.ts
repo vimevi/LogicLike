7ac9a2f0fb8fca53e31ed96bc65a3771123946d1
Maxim Toropchin <vimevi57@gmail.com>
2024-05-25 13:37:52 +0300

    error catcher added

--- LogicLike/src/service/get-data.ts
+++ LogicLike/src/service/get-data.ts
@@ -1,5 +1,9 @@
 export default async function () {
-	const data = await fetch('https://logiclike.com/docs/courses.json');
-	const res = await data.json();
-	return res
+	try {
+		const data = await fetch('https://logiclike.com/docs/courses.json');
+		const res = await data.json();
+		return res;
+	} catch (err) {
+		console.error(err);
+	}
 }
